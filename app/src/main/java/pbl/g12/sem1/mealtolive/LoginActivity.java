3dbf81bb4239cd1076b4beb433ed6ca72cdb168f
archonxgames <archonxgames@gmail.com>
2017-11-28 21:15:38 +0800

    Project Files

--- app/src/main/java/pbl/g12/sem1/mealtolive/LoginActivity.java
+++ app/src/main/java/pbl/g12/sem1/mealtolive/LoginActivity.java
@@ -21,158 +21,173 @@ import com.google.firebase.auth.FirebaseUser;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private static final String TAG = "LoginActivity";
-    private static final int REQUEST_SIGN_UP = 0;
+public class LoginActivity extends AppCompatActivity
+{
+	private FirebaseAuth mAuth;
+	private static final String TAG = "LoginActivity";
+	private static final int REQUEST_SIGN_UP = 0;
 
-    @InjectView(R.id.input_email) EditText _emailText;
-    @InjectView(R.id.input_password) EditText _passwordText;
-    @InjectView(R.id.btn_login) Button _loginButton;
-    @InjectView(R.id.link_signup) TextView _sign_upLink;
+	@InjectView(R.id.input_email)
+	EditText _emailText;
+	@InjectView(R.id.input_password)
+	EditText _passwordText;
+	@InjectView(R.id.btn_login)
+	Button _loginButton;
+	@InjectView(R.id.link_signup)
+	TextView _sign_upLink;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        ButterKnife.inject(this);
-        mAuth = FirebaseAuth.getInstance();
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		ButterKnife.inject(this);
+		mAuth = FirebaseAuth.getInstance();
 
-        _loginButton.setOnClickListener(new View.OnClickListener() {
+		_loginButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				login();
+			}
+		});
 
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
+		_sign_upLink.setOnClickListener(new View.OnClickListener()
+		{
 
-        _sign_upLink.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				// Start the Signup activity
+				Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+				startActivityForResult(intent, REQUEST_SIGN_UP);
+			}
+		});
+	}
 
-            @Override
-            public void onClick(View v) {
-                // Start the Signup activity
-                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                startActivityForResult(intent, REQUEST_SIGN_UP);
-            }
-        });
-    }
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+		// Check if user is signed in (non-null) and update UI accordingly.
+		FirebaseUser currentUser = mAuth.getCurrentUser();
+		// TODO: Insert UI Update if there is a user already logged in
+	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        // TODO: Insert UI Update if there is a user already logged in
-    }
+	public void login()
+	{
+		Log.d(TAG, "Login");
 
-    public void login() {
-        Log.d(TAG, "Login");
+		if (!validate())
+		{
+			onLoginFailed();
+			return;
+		}
 
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
+		_loginButton.setEnabled(false);
 
-        _loginButton.setEnabled(false);
+		final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme_Dark_Dialog);
+		progressDialog.setIndeterminate(true);
+		progressDialog.setMessage("Authenticating...");
+		progressDialog.show();
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
+		String email = _emailText.getText().toString();
+		String password = _passwordText.getText().toString();
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+		mAuth.signInWithEmailAndPassword(email, password)
+				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+				{
+					@Override
+					public void onComplete(@NonNull Task<AuthResult> task)
+					{
+						if (task.isSuccessful())
+						{
+							onLoginSuccess();
+							// Sign in success, update UI with the signed-in user's information
+							// TODO: Insert UI Update if login success
+						} else
+						{
+							// If sign in fails, display a message to the user.
+							onLoginFailed(task);
+							// TODO: Insert UI Update if login fails
+						}
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
-                {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful())
-                        {
-                            onLoginSuccess();
-                            // Sign in success, update UI with the signed-in user's information
-                            // TODO: Insert UI Update if login success
-                        }
-                        else
-                        {
-                            // If sign in fails, display a message to the user.
-                            onLoginFailed(task);
-                            // TODO: Insert UI Update if login fails
-                        }
-
-                        // [START_EXCLUDE]
-                        progressDialog.hide();
-                        // [END_EXCLUDE]
-                    }
-                });
-    }
+						// [START_EXCLUDE]
+						progressDialog.hide();
+						// [END_EXCLUDE]
+					}
+				});
+	}
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SIGN_UP) {
-            if (resultCode == RESULT_OK) {
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (requestCode == REQUEST_SIGN_UP)
+		{
+			if (resultCode == RESULT_OK)
+			{
 
-                // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
-                this.finish();
-            }
-        }
-    }
+				// TODO: Implement successful signup logic here
+				// By default we just finish the Activity and log them in automatically
+				this.finish();
+			}
+		}
+	}
 
-    @Override
-    public void onBackPressed() {
-        // disable going back to the MainActivity
-        moveTaskToBack(true);
-    }
+	@Override
+	public void onBackPressed()
+	{
+		// disable going back to the MainActivity
+		moveTaskToBack(true);
+	}
 
-    public void onLoginSuccess()
-    {
-        Log.d(TAG, "signInWithEmail:success");
-        FirebaseUser user = mAuth.getCurrentUser();
-        finish();
-    }
+	public void onLoginSuccess()
+	{
+		Log.d(TAG, "signInWithEmail:success");
+		FirebaseUser user = mAuth.getCurrentUser();
+		finish();
+	}
 
-    public void onLoginFailed(@NonNull Task<AuthResult> task)
-    {
-        Log.w(TAG, "signInWithEmail:failure", task.getException());
-        Toast.makeText(LoginActivity.this, "Authentication failed.",
-                Toast.LENGTH_SHORT).show();
+	public void onLoginFailed(@NonNull Task<AuthResult> task)
+	{
+		Log.w(TAG, "signInWithEmail:failure", task.getException());
+		Toast.makeText(LoginActivity.this, "Authentication failed.",
+				Toast.LENGTH_SHORT).show();
 
-        _loginButton.setEnabled(true);
-    }
+		_loginButton.setEnabled(true);
+	}
 
-    public void onLoginFailed()
-    {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+	public void onLoginFailed()
+	{
+		Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
-        _loginButton.setEnabled(true);
-    }
+		_loginButton.setEnabled(true);
+	}
 
-    public boolean validate() {
-        boolean valid = true;
+	public boolean validate()
+	{
+		boolean valid = true;
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+		String email = _emailText.getText().toString();
+		String password = _passwordText.getText().toString();
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
-        {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        }
-        else
-            _emailText.setError(null);
+		if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+		{
+			_emailText.setError("enter a valid email address");
+			valid = false;
+		} else
+			_emailText.setError(null);
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10)
-        {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        }
-        else
-            _passwordText.setError(null);
+		if (password.isEmpty() || password.length() < 4 || password.length() > 10)
+		{
+			_passwordText.setError("between 4 and 10 alphanumeric characters");
+			valid = false;
+		} else
+			_passwordText.setError(null);
 
-        return valid;
-    }
+		return valid;
+	}
 }
 
