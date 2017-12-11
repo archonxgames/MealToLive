a87e7aca5b209cc94168362e9957edbb5ee8c9cb
Kevin Xu <mixwhite04@ymail.com>
2017-12-11 18:56:29 +0800

    changed the front end paki check nga po sir

---
+++ app/src/main/java/pbl/g12/sem1/mealtolive/SignupOrganizationActivity.java
@@ -0,0 +1,13 @@
+package pbl.g12.sem1.mealtolive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupOrganizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_organization);
+    }
+}
